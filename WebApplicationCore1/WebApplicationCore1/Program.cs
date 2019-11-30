eae427e122277eb1efc8e70ddf2d7a195276ae32
Bernardas <bzxmixrox@gmail.com>
2019-11-30 20:48:01 +0200

    .

--- WebApplicationCore1/WebApplicationCore1/Program.cs
+++ WebApplicationCore1/WebApplicationCore1/Program.cs
@@ -16,7 +16,7 @@ namespace WebApplicationCore1
         {
             CreateWebHostBuilder(args).Build().Run();
         }
-
+           
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
