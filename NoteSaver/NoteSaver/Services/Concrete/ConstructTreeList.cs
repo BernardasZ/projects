56a06613abc7fcde35f82b739a0e6fcc59b5bae9
Bernardas <bzxmixrox@gmail.com>
2019-12-09 00:00:30 +0200

    ..

---
+++ NoteSaver/NoteSaver/Services/Concrete/ConstructTreeList.cs
@@ -0,0 +1,44 @@
+﻿using DataBaseModel;
+using NoteSaver.Models;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteSaver.Services.Concrete
+{
+    public class ConstructTreeList : IConstructTreeList
+    {
+        private readonly NoteSaverEntities _context;
+        private readonly ILogger _log;
+        public ConstructTreeList(NoteSaverEntities noteSaverEntities)
+        {
+            this._log = Log.ForContext<ConstructTreeList>();
+            this._context = noteSaverEntities;
+        }
+
+        public List<TreeListModel> Construct()
+        {
+            var treeList = _context.ParagraphTreeList.Select(x => x).ToList();
+
+            return null;
+        }
+
+        public bool SaveTreeList(List<TreeListModel> list)
+        {
+            try
+            {
+                CheckTreeForChanges(list);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex, "");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
