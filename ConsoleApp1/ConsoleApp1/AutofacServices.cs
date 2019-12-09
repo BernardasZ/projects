65515b2780dde13e4e720fea8e91b4f07e55eec9
Bernardas <bzxmixrox@gmail.com>
2019-12-09 23:12:15 +0200

    Open/Close principle

---
+++ ConsoleApp1/ConsoleApp1/AutofacServices.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AutofacServices : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            //  Su moduliu neveikia IEnumerable
+
+            builder.RegisterType<RegistrationPreCheck1>().As<IRegistrationPreCheck>();
+            builder.RegisterType<RegistrationPreCheck2>().As<IRegistrationPreCheck>();
+            builder.RegisterType<RegistrationPreCheck3>().As<IRegistrationPreCheck>();
+            builder.RegisterType<RegisterService>();
+        }
+    }
+}
